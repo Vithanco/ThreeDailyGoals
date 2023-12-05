b373dea4d957148909588736c819d80e629558b6
Vithanco <klaus@kneupner.de>
2023-12-05 13:53:00 +0100

    Initial Commit

---
+++ Three Daily Goals/Three_Daily_GoalsApp.swift
@@ -0,0 +1,32 @@
+//
+//  Three_Daily_GoalsApp.swift
+//  Three Daily Goals
+//
+//  Created by Klaus Kneupner on 05/12/2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Three_Daily_GoalsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
