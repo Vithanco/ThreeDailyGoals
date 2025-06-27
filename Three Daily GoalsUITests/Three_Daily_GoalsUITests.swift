d3976374ad1fee037eb5af7f2d1b7871c2bd84fd
Klaus Kneupner <kkn@trackunit.com>
2025-06-27 18:14:11 +0200

    all tests seem to work

--- Three Daily GoalsUITests/Three_Daily_GoalsUITests.swift
+++ Three Daily GoalsUITests/Three_Daily_GoalsUITests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+
 @testable import Three_Daily_Goals
 
 // this is not working for some reason see https://stackoverflow.com/questions/33755019/linker-error-when-accessing-application-module-in-ui-tests-in-xcode-7-1
@@ -14,174 +15,109 @@ import XCTest
 func ensureExists(text: String, inApp: XCUIApplication) {
     let predicate = NSPredicate(format: "value CONTAINS '\(text)'")
     let elementQuery = inApp.staticTexts.containing(predicate)
-    XCTAssertTrue (elementQuery.count > 0, "couldn't find \(text)")
+    XCTAssertTrue(elementQuery.count > 0, "couldn't find \(text)")
 }
 
-
 @MainActor final class Three_Daily_GoalsUITests: XCTestCase {
-    
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        
+
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        
+
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-    
+
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-    
+
     func testButtons() throws {
         // UI tests must launch the application that they test.
         let app = launchTestApp()
-        XCTAssertTrue(app.buttons["Add Task"] .exists)
-        XCTAssertTrue(app.buttons["compassCheckButton"] .exists)
-#if os(iOS)
-        XCTAssertTrue(app.buttons["Redo"] .exists)
-        XCTAssertTrue(app.buttons["Undo"] .exists)
-#endif
-#if os(macOS)
-        XCTAssertTrue(app.menuItems["Redo"].exists)
-        XCTAssertTrue(app.menuItems["Undo"].exists)
-#endif
+        XCTAssertTrue(app.buttons["Add Task"].exists)
+        XCTAssertTrue(app.buttons["compassCheckButton"].exists)
+        #if os(iOS)
+            XCTAssertTrue(app.buttons["Redo"].exists)
+            XCTAssertTrue(app.buttons["Undo"].exists)
+        #endif
+        #if os(macOS)
+            XCTAssertTrue(app.menuItems["Redo"].exists)
+            XCTAssertTrue(app.menuItems["Undo"].exists)
+        #endif
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+
     func testInfo() throws {
         // UI tests must launch the application that they test.
         let app = launchTestApp()
         if isLargeDevice {
             ensureExists(text: "Streak", inApp: app)
         }
-#if os(macOS)
-        ensureExists(text: "today", inApp: app)
-#endif
+        #if os(macOS)
+            ensureExists(text: "today", inApp: app)
+        #endif
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
-    
-    func findFirst(string: String, whereToLook: XCUIElementQuery) -> XCUIElement{
+
+    func findFirst(string: String, whereToLook: XCUIElementQuery) -> XCUIElement {
         let list = whereToLook.matching(identifier: string)
         XCTAssertTrue(list.count > 0, "couldn't find \(string)")
         return list.element(boundBy: 0)
     }
-    
-    @MainActor
-    func testTaskLifeCycleMac() async throws {
-        let testString = "test title 45#"
-#if os(macOS)
-        let app = launchTestApp()
-        
-        let firstButton = findFirst(string: "open_LinkedList", whereToLook: app.staticTexts)
-        firstButton.click()
-        
-        //add a new task
-        let addButton = findFirst(string: "addTaskButton", whereToLook: app.buttons)
-        addButton.click()
-        
-        let title = findFirst(string: "titleField", whereToLook:  app.textFields )
-        XCTAssertNotNil(title)
-        title.click()
-        title.clearText()
-        title.typeText(testString)
-        // XCTAssertTrue(title.i)
-        
-        let addTaskWithTitleButton = findFirst(string: "addTaskWithTitleButton", whereToLook: app.buttons)
-        addTaskWithTitleButton.click()
-        
-        
-        // Ensure app.staticTexts["open_LinkedList"] exists; it's the header to the list
-        let listHeader = app.staticTexts["open_LinkedList"] // Adjust the identifier as needed
-        XCTAssertTrue(listHeader.exists, "List header should be visible")
-        
-        
-        // Ensure I can press a close button
-        let closeButton = findFirst(string: "closeButton", whereToLook: app.buttons)
-        XCTAssertTrue(closeButton.exists, "Close button should be found")
-        closeButton.tap()
-        
-        // Find and click on "closed_LinkedList"
-        let closedList = app.staticTexts["closed_LinkedList"] // Adjust if your identifier is different
-        closedList.click()
-        
-        
-        // Find previously closed task in list
-        // Use a unique identifier for finding the task, which might require tracking task names or other identifiers
-        let closedTask = findFirst(string: "linkToTask"+testString, whereToLook:  app.textFields )
-        //   closedTask.waitForExistence(timeout: TimeInterval(1))
-        XCTAssertNotNil(closedTask)
-        closedTask.click()
-        
-        
-        // Swipe left on the closed task
-        closedTask.swipeLeft()
-        
-        
-        // Find destructive delete button
-        let deleteButton = app.buttons["deleteButton"] // Use the actual identifier for your delete button
-        XCTAssertTrue(deleteButton.exists, "Delete button should be found")
-        
-        // Press delete
-        deleteButton.tap()
-        
-        // Ensure task is deleted
-        XCTAssertTrue(!closedTask.exists, "Task should be deleted")
-#endif
-    }
-    
-    
-    @MainActor
-    func testTaskLifeCycleIOS() async throws {
-        let testString = "test title 45#"
-#if os(iOS)
-        let app = launchTestApp()
-        let firstButton = findFirst(string: "open_LinkedList", whereToLook: app.staticTexts)
-        firstButton.tap()
-        
-        let addButton = findFirst(string: "addTaskButton", whereToLook: app.buttons)
-        addButton.tap()
- 
-        let title = findFirst(string: "titleField", whereToLook:  app.textFields )
-        XCTAssertNotNil(title)
-        title.doubleTap()
-        title.clearText()
-        title.typeText(testString)
-        let submit = findFirst(string: "addTaskWithTitleButton", whereToLook: app.buttons)
-        submit.tap()
-        
-        
-        let back = findFirst(string: "Back", whereToLook: app.buttons)
-        back.tap()
-        firstButton.tap()
-        
-        let testTask = findFirst(string: testString, whereToLook: app.staticTexts)// Use actual identifier for the first task
-                XCTAssertTrue(testTask.exists, "First task should be found")
-        testTask.tap()
-        
-        let closeButton = findFirst(string: "closeButton", whereToLook: app.buttons)
-        XCTAssertTrue(closeButton.exists, "Close button should be found")
-        closeButton.tap()
-        
-        back.tap()
-        back.tap()
 
-        let closedList = app.staticTexts["closed_LinkedList"]
-        closedList.tap()
-        let closedTask = findFirst(string: testString, whereToLook:  app.staticTexts )
-        XCTAssertNotNil(closedTask)
-        closedTask.swipeLeft()
-        
-        let deleteButton = app.buttons["deleteButton"]
-        XCTAssertTrue(deleteButton.exists, "Delete button should be found")
-        deleteButton.tap()
-        
-        XCTAssertTrue(!closedTask.exists, "Task should be deleted")
+
+    @MainActor
+    func testTaskLifeCycle() async throws {
+        let testString = "test title 45#"
+    //
+            let app = launchTestApp()
+            let firstButton = findFirst(string: "open_LinkedList", whereToLook: app.staticTexts)
+            firstButton.tap()
+
+            let addButton = findFirst(string: "addTaskButton", whereToLook: app.buttons)
+            addButton.tap()
+
+            let title = findFirst(string: "titleField", whereToLook: app.textFields)
+            XCTAssertNotNil(title)
+            title.doubleTap()
+            title.clearText()
+            title.typeText(testString)
+            let submit = findFirst(string: "addTaskWithTitleButton", whereToLook: app.buttons)
+            submit.tap()
+
+#if os(iOS)
+            let back = findFirst(string: "Back", whereToLook: app.buttons)
+            back.tap()
 #endif
+            firstButton.tap()
+
+            let testTask = findFirst(string: testString, whereToLook: app.staticTexts)  // Use actual identifier for the first task
+            XCTAssertTrue(testTask.exists, "First task should be found")
+            testTask.tap()
+
+            let closeButton = findFirst(string: "closeButton", whereToLook: app.buttons)
+            XCTAssertTrue(closeButton.exists, "Close button should be found")
+            closeButton.tap()
+#if os(iOS)
+            back.tap()
+            back.tap()
+#endif
+            let closedList = app.staticTexts["closed_LinkedList"]
+            closedList.tap()
+            let closedTask = findFirst(string: testString, whereToLook: app.staticTexts)
+            XCTAssertNotNil(closedTask)
+            closedTask.swipeLeft()
+
+            let deleteButton = app.buttons["deleteButton"]
+            XCTAssertTrue(deleteButton.exists, "Delete button should be found")
+            deleteButton.tap()
+
+            XCTAssertTrue(!closedTask.exists, "Task should be deleted")
+      //
     }
-    
-    
+
     @MainActor func testAppStartsEmpty() throws {
         //        let container = sharedModelContainer(inMemory: true)
         //
@@ -189,7 +125,7 @@ func ensureExists(text: String, inApp: XCUIApplication) {
         //
         //        XCTAssertTrue(sut.items.count, 0, "There should be 0 movies when the app is first launched.")
     }
-    
+
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
